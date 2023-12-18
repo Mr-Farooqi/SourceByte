814af06fcb5acd8d20f08d8370429af1b202b387
Adil <md.adil4078600@gmail.com>
2023-12-18 10:38:34 -0800

    first update

---
+++ js/sarch.js
@@ -0,0 +1,9 @@
+function search() {
+    // Get the input value
+    var searchTerm = document.getElementById('searchInput').value.toLowerCase();
+
+    // Perform your search logic here (e.g., filter content based on the search term)
+
+    // For demonstration purposes, let's just log the search term to the console
+    console.log('Search Term:', searchTerm);
+}
